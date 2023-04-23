a2630214cb1f92c724c6e5810e3ae51dd2e0a543
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-23 17:15:38 +0800

    Psuedocode

--- GWA-SORTER-PROJECT.py
+++ GWA-SORTER-PROJECT.py
@@ -1,1 +1,5 @@
+# Open name-and-gwa-list.txt (read) and open aranged-list.txt (append)
+# Make a dictionary of the file content.
+# Sort the dictionary from highest to lowest gwa.
+# Output the name with higest gwa and the higest gwa.
 
