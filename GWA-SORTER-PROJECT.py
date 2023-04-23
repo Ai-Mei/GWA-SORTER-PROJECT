2f2c7e57ec3217bd78ca249b24717e55e4ec39d4
Ai-Mei <purcia.princessmae296@gmail.com>
2023-04-23 19:56:41 +0800

    format the sorted data line by line.

--- GWA-SORTER-PROJECT.py
+++ GWA-SORTER-PROJECT.py
@@ -11,6 +11,18 @@ with open("NAMES-AND-GWA-LIST.txt") as input_file, open("ARANGED-LIST.txt", "a")
 
 # Sort the dictionary from highest to lowest gwa.
     sorted_gwa = sorted(name_gwa.items(), key = lambda x:x[1])
-    print(sorted_gwa)
+    
+
+# Save the data to arranged-list.txt.
+    output_file.write("Name - GWA\n")
+    output_file.write("\n")
+    print("The list of students from highest to lowest GWA: \n")
+
+# Make the dictionary print line by line. 
+    for x in sorted_gwa:
+        print(str(x).replace("'", "").replace("(", "").replace(")", "").replace(", ", "  "))
+        output_file.write(str(x).replace("'", "").replace("(", "").replace(")", "").replace(", ", " - ") + "\n")
+
 # Output the name with higest gwa and the higest gwa.
     print("\nThe student with the higest GWA is:", sorted_gwa[0][0] + "\n" + "The higest GWA is:", sorted_gwa[0][1])
+    output_file.write("\nThe student with the higest GWA is:" + sorted_gwa[0][0] + "\n" + "The higest GWA is:" + sorted_gwa[0][1])
